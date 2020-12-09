1e44241d08b6925968aaaceb1f598f51825c2938
Iago-LC <iagolc9@gmail.com>
2020-12-09 22:45:52 +0100

    Variables declaradas

--- exercicio4.java
+++ exercicio4.java
@@ -4,3 +4,10 @@ import java.util.Scanner;
 
 public class Exercicio9 {
 
+	public static void main(String[] args) {
+        int n;
+        int blanco;
+        int num = 0;
+        int fig = 1;
+        int punto;
+        Scanner sc = new Scanner(System.in);
